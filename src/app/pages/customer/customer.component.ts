55f9c0cd882b2c2b6a5c038a1e210b4f3933300d
layla shams aldeen <layla@nilogy.com>
2020-01-31 15:22:51 +0200

    apit

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -11,74 +11,7 @@ import { LoaderComponent } from '../loader/loader.component';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  meals: any = [
-    {
-      id: 1,
-      name: 'Biryani',
-      image: 'assets/dummy/images/bri.jpg',
-      subcategories: [
-        {id: 1, name: 'Family', price: 500, tax: 50, disc: 0, quantity: 2, mealname: 'Biryani' },
-        {id: 2, name: 'Plate', price: 300, tax: 50, disc: 0, quantity: 1,  mealname: 'Biryani'  },
-        {id: 3, name: 'Half Plate', price: 200, tax: 50, disc: 0, quantity: 1, mealname: 'Biryani'  }
-      ]
-    },
-    {
-    id: 2,
-    name: 'Burger' ,
-    image: 'assets/dummy/images/bur.jpg' ,
-    subcategories: [
-      {id: 1, name: 'Large', price: 100, tax: 20, disc: 0, quantity: 1, mealname: 'Burger'  },
-      {id: 2, name: 'Medium', price: 80, tax: 20, disc: 0, quantity: 1, mealname: 'Burger'  },
-      {id: 3, name: 'Smal', price: 50, tax: 20, disc: 0, quantity: 1, mealname: 'Burger'  }
-    ]
-  },
-    {
-      id: 3,
-      name: 'Pizza',
-      image: 'assets/dummy/images/piz.jpg',
-      tax: 0,
-      disc: 0,
-      quantity: 1,
-      subcategories: []
-    },
-    {
-      id: 4,
-      name: 'Dish4',
-      image: 'assets/dummy/images/bri.jpg',
-      tax: 0,
-      disc: 0,
-      quantity: 1,
-      subcategories: []
-    },
-    {
-      id: 5,
-      name: 'Dish5',
-      image: 'assets/dummy/images/bri.jpg',
-      tax: 0,
-      disc: 0,
-      quantity: 1,
-      subcategories: []
-    },
-    {
-      id: 6,
-      name: 'Dish6',
-      image: 'assets/dummy/images/bri.jpg',
-      tax: 0,
-      disc: 0,
-      quantity: 1,
-      subcategories: []
-    },
-    {
-      id: 7,
-      name: 'Dish7',
-      image: 'assets/dummy/images/bri.jpg',
-      tax: 0,
-      disc: 0,
-      quantity: 1,
-      subcategories: []
-    },
-  ];
-
+  meals: any = [];
   cart: any  = [];
   showcats = false;
   showorders = false;
@@ -91,7 +24,7 @@ export class CustomerComponent implements OnInit {
     private router: Router,
     public api: ApiService
     ) {
-      // this.api.getData('getitems').subscribe(data => {this.meals    = data;  console.log(data, 'LL'); });
+      this.api.getData('getitems').subscribe(data => {this.meals    = data.data;  console.log(data, 'LL'); });
    }
 
   ngOnInit() {
@@ -118,7 +51,4 @@ export class CustomerComponent implements OnInit {
   }
   palceOrder() {
   }
-  // go() {
-  //   this.router.navigate(['loder']);
-  // }
 }
